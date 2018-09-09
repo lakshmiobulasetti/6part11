8c83a19e4e9f021dd2d8b044e1feb5274fb70ec1
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-09-09 19:24:10 +0530

    Update and rename README.md to 106.java

---
+++ 106.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	String a=s.next();
+	String b=s.next();
+	System.out.println(a+b);
+	}
+}
